e855f67cac264d257a88c7e80802c32d772cb263
castell555 <anvkak0813@gmail.com>
2026-04-02 12:35:26 +0900

    week05: user-defined types && memory management v0.5

--- Week05/소스.cpp
+++ Week05/소스.cpp
@@ -3,13 +3,7 @@
 using namespace std;
 
 int main() {
-	/*int humans = 0;
-	int ages[humans];*/
-
-	int age;
-	//int* a = &age; 포인터 변수는 메모리 번지 주소만을 찾는다
-
-
+	int age, totalPrice=0;
 	int humans = 0;
 	cout << "몇 명? ";
 	cin >> humans; //haep memory
@@ -20,14 +14,21 @@ int main() {
 		cin >> age;
 		*(ages + i) = age;
 	}
+	int kid = 5000, adult = 10000, senier = 7000;
 	for (int i = 0; i < humans; i++) {
-		cout << *(ages + i) << '\n';
+		//cout << *(ages + i) << '\n';
+		age = *(ages + i);
+		if (age >= 65) {
+			totalPrice = totalPrice + senier;
+		}
+		else if (age >= 19) {
+			totalPrice = totalPrice + adult;
+		}
+		else{
+			totalPrice = totalPrice + kid;
+		}
 	}
-
-	/*cout << ages << '\n';
-	cout << &humans << '\n';
-	cout << &age << '\n';*/
-
+	cout << "총 요금 : " << totalPrice << '\n';
 	delete[] ages; // free memory
 
 	return 0;
