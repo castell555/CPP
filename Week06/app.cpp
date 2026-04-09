8bff6bc692938887517f047c35bb5a1ffe098ca5
castell555 <anvkak0813@gmail.com>
2026-04-09 12:24:31 +0900

    Week06: user-defined types && memory management v0.10

--- Week06/app.cpp
+++ Week06/app.cpp
@@ -1,7 +1,12 @@
 //stack memory : parameters, local variables
 //static(data) memory : global object, static object
 // heap memory : dynamic memory allocation (allocate at running time)
-
+/*
+포인터 변수는 메모리 번지 주소만 받을 수 있고 동적 할당하면 힙 메모리 주소가 리턴되서 포인터 변수가 필요
+힙메모리는 러닝타임에 사용, 제거할 때 사용
+힙메모리는 속도 느림
+new 키워드는 자바, C# 사용, delete 안하는 이유는 jvm, 닷넷프레임워크 gc때문
+*/
 #include <iostream>
 using namespace std;
 
@@ -93,7 +98,8 @@ int main()
     Circle* circle8 = test();
     cout << circle8->getRadius() << '\n';
     cout << circle8->getArea() << '\n';
-    delete circle8;
+    delete circle8; // 힙메모리에 할당된 객체를 삭제(소멸자 동작)
+    circle8 = nullptr; // 포인터가 가지고 있던 힙메모리 번지 주소를 0으로 초기화
 
     cout << Circle::getCount() << "개\n";
 
