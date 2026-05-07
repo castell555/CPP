985e73f67152a3835a66da82b19f91f6bcd676dd
castell555 <anvkak0813@gmail.com>
2026-05-07 14:08:27 +0900

    Week10: string v2.6

--- Week10/app02.cpp
+++ Week10/app02.cpp
@@ -2,30 +2,22 @@
 #include <iostream>
 using namespace std;
 
-int myStrcmp(const char* a, const char* b) {
-	int i = 0;
-	while (a[i] != '\0' && b[i] != '\0') {
-		if (a[i] != b[i])  // 문자열이 다르다
-			if (a[i] - b[i] < 0)
-				return -1;
-			else if (a[i] - b[i] > 0)
-				return 1;
-		i++;
-	}
-	if (a[i] < b[i])
-		return -1;
-	else if (a[i] > b[i])
-		return 1;
-	else
-		return 0; // 문자열이 같다.
-}
+
 int main()
 {
-	const char* s1 = "Hi Daelim";
-	const char* s2 = "Hi Daelim";
-	cout << strcmp(s1, s2) << endl;
-	//cout << strncmp(s1, s2, 2) << endl;
-	cout << myStrcmp(s1, s2) << endl;
+	string s1;
+	s1 = "abc";
+	cout << s1 << "/" << s1.size() << endl;
+	cout << s1 << "/" << s1.size() << endl;
+	s1 = "defghijk";
+	cout << s1 << "/" << s1.size() << endl;
+	cout << s1 << "/" << s1.size() << endl;
+	s1 = "123456789012345";
+	cout << s1 << "/" << s1.size() << endl;
+	cout << s1 << "/" << s1.size() << endl;
+	s1 = "1234567890123456";
+	cout << s1 << "/" << s1.size() << endl;
+	cout << s1 << "/" << s1.size() << endl;
 
 	return 0;
 }
