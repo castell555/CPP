f38bca7145541ab93f73cde69538a8329365a6e3
castell555 <anvkak0813@gmail.com>
2026-05-07 11:19:25 +0900

    Week10: string v2.4

--- Week10/app.cpp
+++ Week10/app.cpp
@@ -5,13 +5,18 @@ using namespace std;
 
 int main()
 {
-    //char* p = "ACE"; // string literal, error!!
-    const char* p = "ACE"; // string literal, const, read only
-    cout << p << '\m';
+    int i;
+    cin >> i;
+    int* a = new int[i];
 
-    char a[] = "HI"; //compact initializer
-    const char b[] = "HI"; //compact initializer
-    const char* c = "HI"; // string literal
+    a[0] = 'A';
+    a[1] = NULL;
+
+    cout << a << '\n';
+    cout << a[0] << '\n';
+
+    delete[] a;
+    a = nullptr;
 
     return 0;
 }
