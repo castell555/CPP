b881d88b72122028ddd353b8676b5f1b2f4d7fa7
castell555 <anvkak0813@gmail.com>
2026-05-14 11:36:39 +0900

    Week11 boolalpha v0.2

--- Week11/app.cpp
+++ Week11/app.cpp
@@ -1,4 +1,4 @@
-#include<cstring>
+#include<string>
 #include<iostream>
 #include <iomanip>
 using namespace std;
@@ -12,7 +12,13 @@ int main() {
 	cout << noboolalpha;
 	cout << (s1 == s2) << endl;
 
-
+	cin >> s2;
+	cout << s2 << endl;
+	//getline(cin, s2, ' ');
+	//남아있는 더미값을 제거하기 위해 버퍼를 비워야 한다
+	cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	getline(cin, s2);
+	cout << s2 << endl;
 
 	return 0;
 }
